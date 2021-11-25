73ff9fdb9d950c97f66732fc120fed7a41121dc8
jenns93 <jjenns93@gmail.com>
2021-11-25 03:28:57 +0000

    prepare to deploy to Heroku

---
+++ todo/tests_views.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from .models import Item
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Todo/Todo_list.html')
+
+    def test_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Todo/add_item.html')
+
+    def test_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Todo/edit_item.html')
+
+    def test_can_add_item(self):
+        response = self.client.post('/add', {'name': 'Test added item'})
+        self.assertRedirects(response, '/')
+
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/delete/{item.id}')
+        self.assertRedirects(response, '/')
+        existing_items = Item.object.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
+
+    def test_can_toggle_item(self):
+        item = Item.objects.create(name='Test Todo Item', done=True)
+        response = self.client.get(f'/toggle/{item.id}')
+        self.assertRedirects(response, '/')
+        upated_item = Item.objects.get(id=item.id)
+        self.assertFalse(upated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated name'})
+        self.assertRedirects(response, '/')
+        upated_item = Item.objects.get(id=item.id)
+        self.assertEqual(upated_item.name, 'Updated name')
